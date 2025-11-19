81fe0909e756218a9666a951e77a7f56c5dbeef7
JimmyChhan <201966608@collegeahuntsic.qc.ca>
2025-11-19 15:27:39 -0500

    US-005/005-1 : Créer le formulaire de contact - adapter en statique (#9)

--- get_tuteur.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=tutorat_db;charset=utf8mb4", "root", "");
-
-$stmt = $pdo->query("SELECT id_tuteur, nom, prenom FROM tuteurs");
-
-$tuteurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($tuteurs);
-?>
